d2ed2b699bd5b76d8058f5a860ffde6abd447cae
qinbaochen <1575117217@qq.com>
2020-01-15 15:50:34 +0800

    flyway

---
+++ src/main/resources/db/migration/V1__Create_user_table.sql
@@ -0,0 +1,10 @@
+create table USER
+(
+
+    ID           int auto_increment primary key NOT NULL ,
+    ACCOUNT_ID   VARCHAR(100),
+    NAME         VARCHAR(50),
+    TOKEN        CHAR(36),
+    GMT_CREATE   BIGINT,
+    GMT_MODIFIED BIGINT,
+);
